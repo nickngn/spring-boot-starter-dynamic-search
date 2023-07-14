de0769a95f2db9710f585a99ea270f32062c45e7
Hung, Nguyen Duy <hungnd66@fsoft.com.vn>
2023-07-14 19:16:36 +0800

    Modify sonarcloud config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,16 @@ plugins {
 	`maven-publish`
 	id("org.springframework.boot") version "3.1.1"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("org.sonarqube") version "4.2.1.3168"
 }
 
+sonar {
+	properties {
+		property("sonar.projectKey"), "nickngn_spring-boot-starter-dynamic-search"
+		property("sonar.organization"), "afarmerlearntocode"
+		property("sonar.host"), "https://sonarcloud.io"
+	}
+}
 group = "com.nickngn"
 version = "0.0.1-SNAPSHOT"
 
